0fd7f41ea812ca191b6eaaf0aae9df93700fb54d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-09 22:47:41 +0530

    setting up header navigation buttons and sign in/sign out buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route , Link } from 'react-router-dom';
 import CommentList from './CommentList';
 import CommentBox from './CommentBox';
 
+import { connect } from 'react-redux';
+import { authChange } from '../actions/index';
 
 class App extends Component {
+
+  renderButton(){
+    if(this.props.auth) {
+      return (
+        <button>
+          Sign Out
+        </button>
+      )
+    } else {
+      return (
+        <button>
+          Sign In
+        </button>
+      )
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    )
+  }
+
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route exact path="/" component={CommentList} />
       </div>
@@ -15,4 +49,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToprops = (state) => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToprops, { authChange })(App);
